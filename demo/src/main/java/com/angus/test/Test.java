5274666463174953b8c55c85fcd6573349f17333
Angus <angusric16@gmail.com>
2020-01-14 20:29:15 +0800

    spring framework 5.1 init

---
+++ demo/src/main/java/com/angus/test/Test.java
@@ -0,0 +1,11 @@
+package com.angus.test;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
